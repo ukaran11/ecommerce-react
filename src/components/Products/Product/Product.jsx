c6ed2a704a3f539a936cf3f83955f6841ec06974
ukaran11 <ukaran11@gmail.com>
2021-09-20 23:24:08 +0530

    Utsav | Added the Products

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+const Product = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default Product;
