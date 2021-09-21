8d35fdb9cbb45b77b5a1aa780323232a53ba4005
ukaran11 <ukaran11@gmail.com>
2021-09-21 22:45:16 +0530

    Utsav | Added Navbar and associated styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
-import Products from './components/Products/Products';
+import { Products, Navbar } from './components';
+// import Products from './components/Products/Products';
 
 const App = () => {
     return (
         <div>
+            <Navbar />
             <Products/>
         </div>
     );
