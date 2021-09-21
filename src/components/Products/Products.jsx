ffdc10806b499a3b3038e17ced07303ab508a49a
ukaran11 <ukaran11@gmail.com>
2021-09-21 22:49:05 +0530

    Utsav | Added styling to Products Component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Grid} from '@material-ui/core';
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     {id: 1, name:'Shoes', description: 'Running Shoes', price:'$5', image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fshoes&psig=AOvVaw2MGMIk3v15IqWAlz9v2PWf&ust=1632251359234000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIC1x6WgjvMCFQAAAAAdAAAAABAN'},
@@ -8,8 +9,11 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useStyles();
+
     return(
-    <main>
+    <main className={classes.content}>
+        <div className={classes.toolbar}/>
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
