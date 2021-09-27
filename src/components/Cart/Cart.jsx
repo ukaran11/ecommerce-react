5f3e9dcaeb321823ffd951f887dcb9a9131fca2c
ukaran11 <ukaran11@gmail.com>
2021-09-27 20:12:17 +0530

    Utsav | Created the cart comp

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Typography, Button, Grid } from '@material-ui/core'; 
+
+const Cart = ({ cart }) => {
+    const isEmpty = true;
+
+    const EmptyCart = () => (
+        <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
+    );
+
+    const FilledCart = () => (
+        <>
+            <Grid container spacing={3}></Grid>
+        </>
+    )
+
+    return (
+        <div>
+            <Container>
+                <div className={classes.toolbar} />
+                <Typography className={classes.title} variant="h3" >Your Shopping Cart</Typography>
+                { isEmpty ? <EmptyCart /> : <FilledCart />}
+            </Container>
+        </div>
+    )
+}
+
+export default Cart;
