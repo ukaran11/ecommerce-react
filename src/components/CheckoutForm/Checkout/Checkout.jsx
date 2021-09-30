28aec135950b300ac9a22d5a93b9160e21373576
ukaran11 <ukaran11@gmail.com>
2021-09-30 21:42:18 +0530

    Utsav | Added Address,Payment & Confirmation forms

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -3,12 +3,23 @@ import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider,
 import { classes } from 'istanbul-lib-coverage';
 
 import useStyles from './styles';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = () => {
     const classes = useStyles();
     const [activeStep, setActiveStep] = useState(0);
+
+    const Confirmation = () => (
+        <div>
+            Confirmation
+        </div>
+    )
+
+    const Form = () => activeStep === 0 ? <AddressForm /> : <PaymentForm />
+
 
     return (
         <>
@@ -23,6 +34,7 @@ const Checkout = () => {
                             </Step>
                         ))}
                     </Stepper>
+                    {activeStep === steps.length ? <Confirmation /> : <Form />}
                 </Paper>
             </main>
         </>
