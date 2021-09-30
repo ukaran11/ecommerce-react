38af0a0b0cbd443c73c9cf5d5af7e5100214ee12
ukaran11 <ukaran11@gmail.com>
2021-09-30 14:38:59 +0530

    Utsav | Added functionality to cart buttons

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core'; 
+import { Link } from 'react-router-dom';
 
 import useStyles from './styles';
 import CartItem from './Cartitem/CartItem';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
     const classes = useStyles();
     console.log("cart in cart component",cart) 
 
     const EmptyCart = () => (
-        <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
+        <Typography variant="subtitle1">
+            You have no items in your shopping cart, 
+            <Link to='/' className={classes.link}>start adding some</Link>!
+        </Typography>
     );
 
     const FilledCart = () => (
@@ -17,7 +21,10 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 { cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <CartItem item={item} />
+                        <CartItem 
+                            item={item} 
+                            onUpdateCartQty={handleUpdateCartQty}
+                            onRemoveFromCart={handleRemoveFromCart}/>
                     </Grid>
                 ))}
             </Grid>
@@ -29,7 +36,8 @@ const Cart = ({ cart }) => {
                             size="large" 
                             type="button"
                             variant="contained"
-                            color="secondary">
+                            color="secondary"
+                            onClick={handleEmptyCart}>
                                 Empty Cart
                         </Button>
                         <Button 
