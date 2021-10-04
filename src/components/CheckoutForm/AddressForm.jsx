15475afc05e305dbc061d069aa9d83cdeaffd519
ukaran11 <ukaran11@gmail.com>
2021-10-04 23:26:01 +0530

    Utsav | Added AddressForm & CustomTextField

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+import FormInput from './CustomTextField';
 
 const AddressForm = () => {
+    const methods = useForm();
+
     return (
-        <div>
-            Address Form
-        </div>
-    )
-}
+        <>
+            <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+            <FormProvider {...methods}>
+                <form onSubmit=''>
+                    <Grid container spacing={3}>
+                        <FormInput required name='firstName' label='First Name' />
+                    </Grid>
+                </form>
+            </FormProvider>
+        </>
+)}
 
 export default AddressForm
